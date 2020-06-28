333ac6bdae3c9103284df34b7a7e9cd778bbc51a
jlferrete <joseluisferrete@gmail.com>
2020-06-28 07:02:19 +0200

    Added Observable Stream

---
+++ src/main/java/com/sdos/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,26 @@
+package com.sdos.springboot.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+		
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego", "Juan")
+				.doOnNext(System.out::println);
+		
+	nombres.subscribe();
+	
+	}
+
+}
